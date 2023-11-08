f2031eb0af680680a49837bbade978d7929df581
rodrigo.cloureiro <rodrigocostaloureiro563@gmail.com>
2023-11-08 01:45:15 -0300

    post e get implementado

---
+++ src/main/java/com/example/studentsystem/model/Student.java
@@ -0,0 +1,23 @@
+package com.example.studentsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter@Setter
+    private int id;
+    @Getter@Setter
+    private String name;
+    @Getter@Setter
+    private String address;
+
+    public Student() {
+    }
+}
---
+++ studentfrontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Appbar from './components/Appbar';
+import Student from './components/Student';
+
+function App() {
+  return (
+    <>
+    <Appbar />
+    <Student />
+    </>
+  )
+}
+
+export default App;
